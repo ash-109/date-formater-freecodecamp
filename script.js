af6b30d6dff222e130acf7572cd4abdd76145b32
Ashvin Antyah <a.antyah@gmail.com>
2024-04-17 10:04:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,10 +16,10 @@ dateOptionsSelectElement.addEventListener("change", () => {
       currentDateParagraph.textContent = formattedDate
         .split("-")
         .reverse()
-        .join("-");
+        .join(" - ");
       break;
       case "mm-dd-yyyy-h-mm": 
-      currentDateParagraph.textContent =`${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`
+      currentDateParagraph.textContent =`${month} - ${day} - ${year}   |  ${hours} Hours ${minutes} Minutes`
        break
        default : currentDateParagraph.textContent = formattedDate
      }
